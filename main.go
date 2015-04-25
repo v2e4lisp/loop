542aa4d8a1bcd9d571dce4ce3d37358888f16383
wenjun.yan <mylastnameisyan@gmail.com>
2015-04-25 19:08:11 +0900

    looooooooop

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+        "flag"
+        "fmt"
+        "os"
+        "os/exec"
+        "time"
+)
+
+var (
+        t int
+)
+
+func main() {
+        flag.IntVar(&t, "t", 5, "loop interval second")
+        flag.Parse()
+        args := flag.Args()
+
+        span := time.Duration(t)
+        var cmd *exec.Cmd
+
+        for {
+                <-time.After(span * time.Second)
+                if len(args) > 0 {
+                        cmd = exec.Command(args[0], args[1:]...)
+                        cmd.Stdout = os.Stdout
+                        cmd.Stderr = os.Stderr
+                        if err := cmd.Run(); err != nil {
+                                fmt.Println(err)
+                                os.Exit(1)
+                        }
+                }
+        }
+}
