a0655c9ae1965bce1b4ac49b61447c37894d41ff
wenjun.yan <mylastnameisyan@gmail.com>
2015-04-27 00:12:09 +0900

    add option `s' to trap SIGTERM ...

--- main.go
+++ main.go
@@ -5,20 +5,39 @@ import (
         "fmt"
         "os"
         "os/exec"
+        "os/signal"
+        "syscall"
         "time"
 )
 
 var (
         n float64
+        s bool
 )
 
 func main() {
+        flag.Usage = func() {
+                fmt.Println("loop [-n [interval secs]] [-s] command")
+                fmt.Println("\nOPTIONS:")
+                flag.PrintDefaults()
+        }
         flag.Float64Var(&n, "n", 2, "loop interval second")
+        flag.BoolVar(&s, "s", false, "trap SIGTERM SIGINT SIGHUP")
         flag.Parse()
         args := flag.Args()
 
         span := time.Duration(n)
         var cmd *exec.Cmd
+
+        if s {
+                sigs := make(chan os.Signal, 1)
+                signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+                go func() {
+                        for {
+                                fmt.Println("Received:", <-sigs)
+                        }
+                }()
+        }
 
         for {
                 if len(args) == 1 {
